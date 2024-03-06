3f38af573625c678ee5df10786bd752af3caeb46
YolooCode <liut1258@gmail.com>
2024-03-06 13:15:09 +0200

    Updated movment(rotation with A-D)

--- Game3D/Source.cpp
+++ Game3D/Source.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <Windows.h>
+#include <chrono>
 
 using namespace std;
 
 int screenWidth = 120;
 int screenHeight = 40;
 
-float PlayersX = 0.0f;
-float PlayersY = 0.0f;
-float PlayersZ = 0.0f;
+float PlayersX = 8.0f;
+float PlayersY = 8.0f;
+float PlayersZ = 8.0f;
 
 
 int mapHeight = 16;
@@ -45,8 +46,28 @@ int main() {
 	map += L"#..............#";
 	map += L"################";
 
+
+	auto tp1 = chrono::system_clock::now();
+	auto tp2 = chrono::system_clock::now();
+
 	while (true) 
 	{
+
+		auto tp2 = chrono::system_clock::now();
+		chrono::duration<float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float elapsedTimefloat = elapsedTime.count();
+
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
+		{
+			PlayersZ -= 0.1f * elapsedTimefloat;
+		}
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
+		{
+			PlayersZ += 0.1f * elapsedTimefloat;
+		}
+
+
 
 		for (int x = 0; x < screenWidth; x++)
 		{
@@ -87,6 +108,24 @@ int main() {
 
 			int celling = (float)(screenHeight / 2.0) - screenHeight / ((float)distanceToWall);
 			int floor = screenHeight - celling;
+
+			for (int y = 0; y < screenHeight; y++) 
+			{
+				if (y < celling) 
+				{
+					screen[y * screenWidth + x] = ' ';
+				}
+				else if(y > celling && y <= floor)
+				{
+					screen[y * screenWidth + x] = '#';
+				}
+				else
+				{
+					screen[y * screenWidth + x] = ' ';
+				}
+
+
+			}
 
 
 		}
